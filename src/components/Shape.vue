aa106a52fa76c7b2af390c4d7411932e02879633
Teen Yang <chopcostop@gmail.com>
2021-12-16 16:20:45 +0800

    add: component of shape item

---
+++ src/components/Shape.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Shape" @click="$emit('click')">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Shape",
+};
+</script>
+
+<style lang="scss" scoped>
+.Shape {
+  width: 60px;
+  height: 60px;
+
+  display: inline-block;
+
+  font-size: 30px;
+
+  &:hover {
+    border-radius: 10px;
+    background-color: lightgray;
+    cursor: pointer;
+  }
+
+  & > * {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
