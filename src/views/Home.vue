5c02e6aa072a914c32f10a21b3cfe5ab0f49f997
Teen Yang <chopcostop@gmail.com>
2021-12-16 16:40:16 +0800

    feat: create, color, drag and save shapes in canvas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,219 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Home">
+    <Controller
+      @update:shape="createShape"
+      @update:color="setColor"
+      @update:clearCanvas="clearCanvas"
+      @update:saveCanvas="saveCanvas"
+    />
+
+    <section class="painting">
+      <canvas
+        id="drawCanvas"
+        width="1000"
+        height="600"
+        ref="canvas"
+        @click="selectElement"
+        @mousedown="onDrag"
+        @mousemove="onDragging"
+        @mouseup="onDrop"
+      ></canvas>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//~ ctx.rect version
+// import drawCtxShape from "@/utility/drawCtxShape.js";
+
+//! Path2D
+import drawShape from "@/utility/drawShape.js";
+import Controller from "@/views/Controller.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Controller,
+  },
+  created() {
+    this.$store.commit(
+      "canvas/shapes",
+      JSON.parse(localStorage.getItem("drawing_shapes")) || []
+    );
+    this.shapes.forEach((shape, index) => {
+      const restoreShape = new drawShape.Shape(
+        shape.type,
+        shape.x,
+        shape.y,
+        shape.width,
+        shape.height
+      );
+      restoreShape.updateColor({
+        strokeColor: shape.strokeColor,
+        fillColor: shape.fillColor,
+      });
+      // 更新原始陣列內的元素資料，重新存入vuex
+      this.shapes.splice(index, 1, restoreShape);
+      this.$store.commit("canvas/shapes", this.shapes);
+    });
+    console.log("created shapes, ", this.shapes);
+  },
+  mounted() {
+    this.canvas = this.$refs.canvas;
+    this.$store.commit("canvas/canvasCtx", this.canvas.getContext("2d"));
+    this.render(); // 遞迴執行window.requestAnimationFrame
+  },
+  data() {
+    return {
+      canvas: null,
+      selectedIndex: null,
+      isDragging: false,
+    };
+  },
+  computed: {
+    ctx() {
+      return this.$store.getters["canvas/canvasCtx"];
+    },
+    shapes() {
+      return this.$store.getters["canvas/shapes"];
+    },
+  },
+  methods: {
+    render() {
+      // 要求瀏覽器在下次重繪畫面前，呼叫特定函數更新動畫
+      window.requestAnimationFrame(() => {
+        this.shapes.forEach((shape) => shape.render(this.ctx));
+        this.render();
+      });
+    },
+
+    clearCanvas() {
+      console.log("clearCanvas");
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.$store.commit("canvas/shapes", []);
+    },
+    saveCanvas() {
+      console.log("saveCanvas");
+      localStorage.setItem("drawing_shapes", JSON.stringify(this.shapes));
+    },
+
+    selectElement(event) {
+      //! Path2D
+      this.shapes.forEach((shape, index) => {
+        if (this.ctx.isPointInPath(shape.path, event.offsetX, event.offsetY)) {
+          // isPointInPath布林結果：指定shape.path路徑內是否包含檢測點offsetX/offsetY
+          // True：點擊範圍內的圖形，即為當前選擇的元素
+          this.selectedIndex = index;
+        }
+      });
+      console.warn("selectElement index: ", this.selectedIndex);
+
+      //~ ctx.rect version
+      // this.findCurrentLayer(event.offsetX, event.offsetY);
+    },
+    onDrag(event) {
+      //! Path2D version
+      this.shapes.forEach((shape, index) => {
+        if (this.ctx.isPointInPath(shape.path, event.offsetX, event.offsetY)) {
+          this.selectedIndex = index;
+          this.isDragging = true;
+          this.canvas.style.cursor = "move";
+        }
+      });
+      console.warn(">>> mouse down index: ", this.selectedIndex);
+
+      //~ ctx.rect version
+      // this.findCurrentLayer(event.offsetX, event.offsetY);
+      // if (this.shapes.some((shape) => shape.isInRange)) {
+      //   this.canvas.style.cursor = "move";
+      //   this.isDragging = true;
+      //   console.warn("(">>>> mouse down isDragging: ", this.isDragging);
+      // }
+    },
+    onDrop() {
+      console.error(">>>>> mouse up");
+      this.isDragging = false;
+      this.canvas.style.cursor = "auto";
+    },
+
+    onDragging(event) {
+      if (!this.isDragging) {
+        return;
+      }
+      // 拖曳同時清空整個畫布，再重新繪製所有圖形
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      console.log("isDragging");
+
+      this.shapes[this.selectedIndex].updatePosition({
+        x: this.shapes[this.selectedIndex].x + event.movementX,
+        y: this.shapes[this.selectedIndex].y + event.movementY,
+      });
+
+      //~ ctx.rect version
+      // this.findCurrentLayer(event.offsetX, event.offsetY);
+    },
+
+    createShape(shape) {
+      const newShape = new drawShape.Shape(
+        shape.text,
+        shape.x,
+        shape.y,
+        shape.width,
+        shape.height
+      );
+      this.$store.commit("canvas/shapes", [...this.shapes, newShape]);
+
+      // 新增的圖形，直接作為當前選擇的元素，可直接套用顏色
+      this.selectedIndex = this.shapes.length - 1;
+      console.warn(
+        "createShape index/shapes: ",
+        this.selectedIndex,
+        this.shapes
+      );
+    },
+    setColor(color) {
+      if (this.shapes.length === 0) {
+        return;
+      }
+
+      this.shapes[this.selectedIndex].updateColor({
+        strokeColor: color.styleName,
+        fillColor: color.styleName,
+      });
+      console.warn("setColor: ", this.selectedIndex, color.styleName);
+    },
+
+    findCurrentLayer(rangeX, rangeY) {
+      //~ ctx.rect version
+      this.shapes.forEach((shape, index) => {
+        shape.range(rangeX, rangeY);
+        if (shape.isInRange) {
+          this.selectedIndex = index;
+        }
+      });
+      console.log("findCurrentLayer selectedIndex: ", this.selectedIndex);
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.Home {
+  margin: 30px;
+  outline: 3px solid gray;
+  display: flex;
+
+  & > * {
+    outline: 1px solid lightgray;
+    padding: 20px;
+
+    flex: 1 0 0;
+  }
+
+  .painting {
+    canvas {
+      outline: 1px dashed black;
+    }
+  }
+}
+</style>
